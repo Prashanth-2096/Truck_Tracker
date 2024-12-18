11a010e13195bafe3990bf8281912d59439c33a2
hanna <hannaabdulmajeed@gmail.com>
2024-12-18 13:20:43 +0400

    Updated detection model

--- backend/detection.py
+++ backend/detection.py
@@ -1,52 +1,101 @@
-import cv2 as cv
+from transformers import pipeline
+from PIL import Image, ImageFilter
+from PIL import ImageDraw
+from paddleocr import PaddleOCR
+import numpy
 
-# OCR using tesserct
-import os
-import glob
-import pytesseract 
-pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'  # Adjust path as necessary
+def extract_license_plate(image_path):
+    checkpoint = "google/owlv2-base-patch16-ensemble"
+    detector = pipeline(
+        model=checkpoint,
+        task="zero-shot-object-detection",
+        device="cpu"
+    )
 
-pth = os.getcwd() + "\\LPs/**/*.jpg"
-listLPs = []
-predictedLPs = []
-for pth in glob.glob(pth, recursive = True):
-    LPfile = pth.split("\\")[-1]    
-    LP, ext = os.path.splitext(LPfile)
+    img = Image.open(f"{image_path}")
 
-    # ppending the license plte to the list of license pltes
-    listLPs.append(LP)
+    # preprocessing imge
+    gry = img.convert('L')
+    blur = gry.filter(ImageFilter.GaussianBlur(1))
+    # thresh = gry.point(lambda p: 255 if p > 127 else 0)
 
-    # now we red the imge using openCV nd then does tesserct on it to get the license plte number
-    img = cv.imread(f'LPs/{LPfile}')
-    resizedImg = cv.resize(img, None, fx=1.25, fy=1.25, interpolation=cv.INTER_CUBIC)
-    gry = cv.cvtColor(resizedImg, cv.COLOR_BGR2GRAY)
-    blur = cv.GaussianBlur(gry, (5,5), 0)
+    prediction = detector(blur,candidate_labels=["truck license plate with english letters and numbers"],)
+    if not prediction:
+        print("No license plates detected.")
+    else:
+        prediction = prediction[0]
+        temporary_image = blur.copy()
+        draw = ImageDraw.Draw(temporary_image)
+        box = prediction["box"]
+        label = prediction["label"]
+        score = prediction["score"]
+        xmin, ymin, xmax, ymax = box.values()
+        draw.rectangle((xmin, ymin, xmax, ymax), outline="red", width=1)
+        draw.text((xmin, ymin), f"{label}: {round(score, 2)}", fill="white")
 
-    # now to get ROI from imge of truck
+        cropped_image = blur.crop(list(box.values()))
 
-    # now do tesserct OCR
-    predictedLP = pytesseract.image_to_string(blur, lang='eng', config='--oem 3 --psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
-    filterPredictedLP = "".join(predictedLP.split()).replace("-","").replace(":","")
-    predictedLPs.append(filterPredictedLP)
-
-# now to print the ctul license plte, predicted license plte nd it's ccurcy
-print("ctul License Plte", "|", "Predicted Licenes Plte", "|", "ccurcy")
-
-def clculteccurcy(ctulList, predictedList):
-    for ctulPlte, predictedPlte in zip(ctulList, predictedList):
-        ccurcy = "0%"
-        numMtches = 0
-
-        if ctulPlte == predictedPlte:
-            ccurcy = "100%"
+        # Now to do OCR with PddleOCR
+        ocr = PaddleOCR(use_angle_cls=True, lang="en",char_type='en')
+        if ocr:
+            print("OCR initialized successfully.")
         else:
-            if len(ctulPlte) == len(predictedPlte):
-                for a, p in zip(ctulPlte, predictedPlte):
-                    if (a == p):
-                        numMtches += 1
-            ccurcy = str(round(100 * (numMtches / len(ctulPlte)), 2))
-            ccurcy += "%"
-        print("    ", ctulPlte, "\t", predictedPlte, "\t    ", ccurcy) 
+            print("Failed to initialize PaddleOCR.")
 
-clculteccurcy(listLPs, predictedLPs)
+        cropped_numpy_image = numpy.array(cropped_image)
+        # cropped_numpy_image_rgb = cropped_numpy_image[:, :, ::-1].copy()
 
+    if cropped_numpy_image is not None:
+        print(cropped_numpy_image.shape)
+    else:
+        print("Input image is None or invalid.")
+
+    result = ocr.ocr(cropped_numpy_image, cls=True)
+
+    # if blur doesnt work, try thresholding the imge
+    if result is None or len(result) == 0 or result[0] is None:
+        thresh = gry.point(lambda p: 255 if p > 127 else 0)
+        prediction = detector(thresh,candidate_labels=["truck license plate with english letters and numbers"],)
+        if not prediction:
+            print("No license plates detected.")
+        else:
+            prediction = prediction[0]
+            temporary_image = thresh.copy()
+            draw = ImageDraw.Draw(temporary_image)
+            box = prediction["box"]
+            label = prediction["label"]
+            score = prediction["score"]
+            xmin, ymin, xmax, ymax = box.values()
+            draw.rectangle((xmin, ymin, xmax, ymax), outline="red", width=1)
+            draw.text((xmin, ymin), f"{label}: {round(score, 2)}", fill="white")
+
+            cropped_image = thresh.crop(list(box.values()))
+
+            # Now to do OCR with PddleOCR
+            ocr = PaddleOCR(use_angle_cls=True, lang="en",char_type='en')
+            if ocr:
+                print("OCR initialized successfully.")
+            else:
+                print("Failed to initialize PaddleOCR.")
+
+            cropped_numpy_image = numpy.array(cropped_image)
+            # not needed since img hs been converted to gry
+            # cropped_numpy_image_rgb = cropped_numpy_image[:, :, ::-1].copy() 
+        if cropped_numpy_image is not None:
+            print(cropped_numpy_image.shape)
+        else:
+            print("Input image is None or invalid.")
+
+        result = ocr.ocr(cropped_numpy_image, cls=True)
+        if result is None or len(result) == 0 or result[0] is None:
+            return None
+        else:
+            texts = [text for box, (text, confidence) in result[0]]
+            predictedLP = ''.join(texts)
+            return predictedLP
+    else:
+        texts = [text for box, (text, confidence) in result[0]]
+        predictedLP = ''.join(texts)
+        return predictedLP
+
+    return None
